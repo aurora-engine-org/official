558bd1bc24758c5055734680a25fe37dd4dc4cda
dzw <zhiwen_der@qq.com>
2022-05-01 13:12:32 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,14 @@
 module.exports = {
     title: 'AURORA',
 
+    //主题 设置
     themeConfig:{
+        //仓库配置
         repo: '/aurora-go/aurora',
+        //仓库分支
         docsBranch: 'master',
         smoothScroll: true,
+        //README 侧边导航配置
         sidebar: {
             '/start/':[
                 '',
